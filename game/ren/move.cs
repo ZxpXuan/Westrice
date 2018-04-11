2a26675cd7fa9ac879d0159c19e34689b7cb6ad8
ZxpXuan <xuanyuning@foxmail.com>
2018-04-11 11:50:30 +0800

    Whole Project

---
+++ game/ren/move.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            gameObject.transform.Translate(Vector3.up* 5 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.Translate(Vector3.down * 5 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.Translate(Vector3.left * 5 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.Translate(Vector3.right * 5 * Time.deltaTime);
+        }
+    }
+}
